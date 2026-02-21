03141ceea35719c69e476694cc6ab22f759a83ca
Mohamed Traore <mohamed.trapro@gmail.com>
2026-02-21 14:20:32 -0500

    Add second max pooling layer to model

--- model.py
+++ model.py
@@ -32,14 +32,17 @@ class Model(nn.Module):
         self.global_avg_pool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(128, num_classes)
         self.dropout = nn.Dropout(0.3)
+        self.pool2 = nn.MaxPool2d(2, 2)
 
     def forward(self, x):
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
         x = self.pool(x)
         x = F.relu(self.bn3(self.conv3(x)))
+        x = self.pool2(x)
         x = self.global_avg_pool(x)
         x = x.view(x.size(0), -1)  # Flatten
         x = self.dropout(x)
         x = self.fc(x)
         return x
+    
